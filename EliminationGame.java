0cdf5749b73f7a4179ee51a037d715e2d1a702ea
zyu539 <zyu539@aucklanduni.ac.nz>
2018-03-09 12:23:39 +1300

    Create EliminationGame.java

---
+++ EliminationGame.java
@@ -0,0 +1,30 @@
+/**
+There is a list of sorted integers from 1 to n. Starting from left to right, remove the first number and every other number afterward until you reach the end of the list.
+
+Repeat the previous step again, but this time from right to left, remove the right most number and every other number from the remaining numbers.
+
+We keep repeating the steps again, alternating left to right and right to left, until a single number remains.
+
+Find the last number that remains starting with a list of length n.
+
+Example:
+
+Input:
+n = 9,
+1 2 3 4 5 6 7 8 9
+2 4 6 8
+2 6
+6
+
+Output:
+6
+
+
+Answer: Given the array, int the first elimination, all odd numbers are deleted, only even number remains.
+Those even numbers are also 2 times as the mirror result of 1 to n/2.
+*/ 
+class EliminationGame {
+    public int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+    }
+}
